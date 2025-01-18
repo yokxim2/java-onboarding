dd5b1f0823bad8d3796d667506e2ee5ebf6627a5
yokxim <yooksi53@gmail.com>
2025-01-18 21:42:09 +0900

    feat(custom-user-details) 인증에 필요한 사용자 정보를 커스터마이징하는 클래스 생성

---
+++ src/main/java/org/example/javaonboarding/global/dto/CustomUserDetails.java
@@ -0,0 +1,32 @@
+package org.example.javaonboarding.global.dto;
+
+import org.example.javaonboarding.domain.user.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
