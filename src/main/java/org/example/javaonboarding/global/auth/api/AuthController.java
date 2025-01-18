e39e157f81b876d0ad202ae423862b102f185e2f
yokxim <yooksi53@gmail.com>
2025-01-19 00:08:38 +0900

    fix(auth-controller) reissue 메서드 수행 후, 리프레시 토큰을 만들어 쿠키에 담음

--- src/main/java/org/example/javaonboarding/global/auth/api/AuthController.java
+++ src/main/java/org/example/javaonboarding/global/auth/api/AuthController.java
@@ -1,13 +1,15 @@
 package org.example.javaonboarding.global.auth.api;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.javaonboarding.global.auth.application.AuthService;
-import org.example.javaonboarding.global.jwt.JWTUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.example.javaonboarding.global.jwt.LoginFilter.COOKIE_EXPIRATION_TIME;
 
 @RestController
 public class AuthController {
@@ -20,15 +22,26 @@ public class AuthController {
 
     @PostMapping("/reissue")
     public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
-        String newAccessToken = null;
+        String newAccessToken, newRefreshToken;
 
         try {
-            newAccessToken = authService.reissue(request.getCookies());
+            String[] tokens = authService.reissue(request.getCookies());
+            newAccessToken = tokens[0];
+            newRefreshToken = tokens[1];
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
 
         response.setHeader("access", newAccessToken);
+        response.addCookie(createCookie("refresh", newRefreshToken));
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Cookie createCookie(String key, String value) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(COOKIE_EXPIRATION_TIME);
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }
