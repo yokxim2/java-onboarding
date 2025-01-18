d2193a7cb8f00428321059c0b8743b75b55ed7fa
yokxim <yooksi53@gmail.com>
2025-01-18 22:56:04 +0900

    fix(user) User 클래스에 Setter 사용

--- src/main/java/org/example/javaonboarding/domain/user/domain/User.java
+++ src/main/java/org/example/javaonboarding/domain/user/domain/User.java
@@ -5,10 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,12 +22,4 @@ public class User {
     private String nickname;
 
     private String role;
-
-    public User(String username, String password, String nickname, String role) {
-        this.username = username;
-        this.password = password;
-        this.nickname = nickname;
-        this.role = role;
-    }
-
 }
