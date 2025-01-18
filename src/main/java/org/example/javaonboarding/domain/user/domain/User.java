9514200605737ade45081ed217a8b43146ee6de8
yokxim <yooksi53@gmail.com>
2025-01-18 20:51:06 +0900

    feat(user) 유저 엔티티 클래스 추가

--- src/main/java/org/example/javaonboarding/domain/user/domain/User.java
+++ src/main/java/org/example/javaonboarding/domain/user/domain/User.java
@@ -5,18 +5,20 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
     private String username;
     private String password;
     private String nickname;
 
     private String role;
+
 }
